bd48d9d48968851af8209aac2a74485221abbe9b
Shahnawaz Khan <56075826+shahnawaz-khn@users.noreply.github.com>
2020-05-28 18:15:04 +0200

    Commenting

--- HouseAndFirstPerson/Assets/Scripts/Cashout.cs
+++ HouseAndFirstPerson/Assets/Scripts/Cashout.cs
@@ -9,34 +9,36 @@ using System;
 
 public class Cashout : MonoBehaviour
 {
+    //Audio Variables
     public AudioSource cashRegSound;
     public AudioSource productSound;
-    Text txt;
-    double bill;
-    string[] purchasedItems = new string[5];
-    MeshCollider rend;
-    GameObject[] allObjects;
-    List<Vector3> paths;
+  
+    Text txt; //speech bubble text change variable
+    double bill; // total product cost variable
+    string[] purchasedItems = new string[5]; //array to store name of purchased itmes
+    MeshCollider rend; //to turn of cloned objects mesh 
+    GameObject[] allObjects; //array to store all gameobjects in the scene
+    List<Vector3> paths; //list to store positions of cloned objects(purchased items)
     GameObject[] checkout;
-    private int count = 0;
+    private int count = 0; //counter to check if the products purchased do not exceed 5
 
     // Start is called before the first frame update
     void Start()
     {
         allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
         paths = new List<Vector3>(); // empty now
-        paths.Add(new Vector3(5.39f, 2.36f, -6.6f));
-        paths.Add(new Vector3(5.39f, 2.36f, -5.8f));
-        paths.Add(new Vector3(5.39f, 2.36f, -5.4f));
-        paths.Add(new Vector3(5.39f, 2.36f, -4.8f));
-        paths.Add(new Vector3(5.39f, 2.36f, -4.2f));
+        paths.Add(new Vector3(5.39f, 2.36f, -6.6f)); //1st Location of purchased item
+        paths.Add(new Vector3(5.39f, 2.36f, -5.8f)); //2nd Location of purchased item
+        paths.Add(new Vector3(5.39f, 2.36f, -5.4f)); //3rd Location of purchased item
+        paths.Add(new Vector3(5.39f, 2.36f, -4.8f)); //4th Location of purchased item
+        paths.Add(new Vector3(5.39f, 2.36f, -4.2f)); //5th Location of purchased item
 
         foreach (GameObject go in allObjects)
         {
-            Debug.Log(go.name);
-            if (go.name == "BubbleHolder")
+            //Debug.Log(go.name);
+            if (go.name == "BubbleHolder") //Searching for speechBubble in all gameObjects available in scene
             {
-                go.SetActive(false);
+                go.SetActive(false); //Disabling it at Start()
             }
         }
     }
@@ -44,128 +46,128 @@ public class Cashout : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) //on mouse click
         {
-            Debug.Log(count);
+            //Debug.Log(count);
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, 100000.0f))
+            if (Physics.Raycast(ray, out hit, 100000.0f)) //Raycast to hit an object
             {
-                if (hit.collider.CompareTag("item1") && count < 5)
+                if (hit.collider.CompareTag("item1") && count < 5) //item hit
                 {
 
                     foreach (GameObject go in allObjects)
                     {
-                        if (go.name == "toblerone")
+                        if (go.name == "toblerone") //name of item
                         {
-                            GameObject item1clone = Instantiate(go) as GameObject;
-                            item1clone.transform.position = paths[count];
+                            GameObject item1clone = Instantiate(go) as GameObject; //cloning item
+                            item1clone.transform.position = paths[count]; // setting position on counter
                             rend = item1clone.GetComponent<MeshCollider>();
-                            rend.enabled = false;
-                            purchasedItems[count] = "Toblerone: €2.00";
-                            bill += 2;
-                            productSound.Play();
+                            rend.enabled = false; //turning collider off
+                            purchasedItems[count] = "Toblerone: €2.00"; // saving item and its price in array
+                            bill += 2; // addition to bill
+                            productSound.Play(); //playing added to cart successfully sound 
                         }
                     }
-                    count += 1;
+                    count += 1; //increasing counter
                 }
-                if (hit.collider.CompareTag("item2") && count < 5)
+                if (hit.collider.CompareTag("item2") && count < 5) //item hit
                 {
 
                     foreach (GameObject go in allObjects)
                     {
-                        if (go.name == "cheezIt")
+                        if (go.name == "cheezIt") //name of item
                         {
-                            GameObject item2clone = Instantiate(go) as GameObject;
-                            item2clone.transform.position = paths[count];
+                            GameObject item2clone = Instantiate(go) as GameObject; //cloning item
+                            item2clone.transform.position = paths[count]; // setting position on counter
                             rend = item2clone.GetComponent<MeshCollider>();
-                            rend.enabled = false;
-                            purchasedItems[count] = "CheezIt: €2.50";
-                            bill += 2.50;
-                            productSound.Play();
+                            rend.enabled = false; //turning collider off
+                            purchasedItems[count] = "CheezIt: €2.50"; // saving item and its price in array
+                            bill += 2.50; // addition to bill
+                            productSound.Play(); //playing added to cart successfully sound 
                         }
                     }
-                    count += 1;
+                    count += 1; //increasing counter
                 }
-                if (hit.collider.CompareTag("item3") && count < 5)
+                if (hit.collider.CompareTag("item3") && count < 5) //item hit
                 {
 
                     foreach (GameObject go in allObjects)
                     {
-                        if (go.name == "soupCan")
+                        if (go.name == "soupCan") //name of item
                         {
-                            GameObject item3clone = Instantiate(go) as GameObject;
-                            item3clone.transform.position = paths[count];
+                            GameObject item3clone = Instantiate(go) as GameObject; //cloning item
+                            item3clone.transform.position = paths[count]; // setting position on counter
                             rend = item3clone.GetComponent<MeshCollider>();
-                            rend.enabled = false;
-                            purchasedItems[count] = "Soup Can: €0.99";
-                            bill += 0.99;
-                            productSound.Play();
+                            rend.enabled = false; //turning collider off
+                            purchasedItems[count] = "Soup Can: €0.99"; // saving item and its price in array
+                            bill += 0.99; // addition to bill
+                            productSound.Play(); //playing added to cart successfully sound 
                         }
                     }
-                    count += 1;
+                    count += 1; //increasing counter
                 }
-                if (hit.collider.CompareTag("item4") && count < 5)
+                if (hit.collider.CompareTag("item4") && count < 5) //item hit
                 {
 
                     foreach (GameObject go in allObjects)
                     {
-                        if (go.name == "redBullCan")
+                        if (go.name == "redBullCan") //name of item
                         {
-                            GameObject item4clone = Instantiate(go) as GameObject;
-                            item4clone.transform.position = paths[count];
+                            GameObject item4clone = Instantiate(go) as GameObject; //cloning item
+                            item4clone.transform.position = paths[count]; // setting position on counter
                             rend = item4clone.GetComponent<MeshCollider>();
-                            rend.enabled = false;
-                            purchasedItems[count] = "RedBull Can: €1.49";
-                            bill += 1.49;
-                            productSound.Play();
+                            rend.enabled = false; //turning collider off
+                            purchasedItems[count] = "RedBull Can: €1.49"; // saving item and its price in array
+                            bill += 1.49; // addition to bill
+                            productSound.Play(); //playing added to cart successfully sound 
                         }
                     }
                     count += 1;
                 }
-                if (hit.collider.CompareTag("item5") && count < 5)
+                if (hit.collider.CompareTag("item5") && count < 5) //item hit
                 {
                     foreach (GameObject go in allObjects)
                     {
-                        if (go.name == "beets")
+                        if (go.name == "beets") //name of item
                         {
-                            GameObject item5clone = Instantiate(go) as GameObject;
-                            item5clone.transform.position = paths[count];
+                            GameObject item5clone = Instantiate(go) as GameObject; //cloning item
+                            item5clone.transform.position = paths[count]; // setting position on counter
                             rend = item5clone.GetComponent<MeshCollider>();
-                            rend.enabled = false;
-                            purchasedItems[count] = "Beets: €1.00";
-                            bill += 1;
-                            productSound.Play();
+                            rend.enabled = false; //turning collider off
+                            purchasedItems[count] = "Beets: €1.00"; // saving item and its price in array
+                            bill += 1; // addition to bill
+                            productSound.Play(); //playing added to cart successfully sound 
                         }
                     }
                     count += 1;
                 }
-                if (hit.collider.CompareTag("register"))
+                if (hit.collider.CompareTag("register")) //click on register
                 {
                     foreach (GameObject go in allObjects)
                     {
-                        if (go.name == "BubbleHolder")
+                        if (go.name == "BubbleHolder") //searching for speechBubble
                         {
 
                             foreach (GameObject g in allObjects)
                             {
                                 Debug.Log(g.name);
 
-                                if (g.name == "Text")
+                                if (g.name == "Text") // Accessing TextField
                                 {
                                     string seperator = "\n";
                                     string bubble;
-                                    string finalBill = "The total cost is: " + bill;
+                                    string finalBill = "The total cost is: " + bill; //Fill total bill
                                     txt = g.GetComponent<Text>();
-                                    bubble = string.Join(seperator, purchasedItems);
-                                    finalBill = bubble + "\n" + finalBill;
-                                    txt.text = finalBill;
-                                    cashRegSound.Play();
+                                    bubble = string.Join(seperator, purchasedItems); //Combining strings
+                                    finalBill = bubble + "\n" + finalBill; // combining strings
+                                    txt.text = finalBill; // chaning speechbubble text
+                                    cashRegSound.Play(); // playing register sound
                                     Debug.Log("Text Changed");
                                     //go.SetActive(false);
                                 }
                             }
-                            go.SetActive(true);
+                            go.SetActive(true); // Enabling speechBubble
                         }
                     }
                 }
